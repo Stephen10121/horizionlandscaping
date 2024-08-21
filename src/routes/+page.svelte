095c56bd6e71750ba71f41b0b6a8e926eb577f1f
Stephen Gruzin <stephengrzn@gmail.com>
2024-08-20 18:22:30 -0700

    Added commands to build and host the site. also showing home page under construction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,13 @@
 </script>
 
 <main>
-    <section>1</section>
+    <!-- <section>1</section>
     <section>2</section>
     <Home />
     <section>4</section>
-    <section>5</section>
+    <section>5</section> -->
+    <p class="dm-serif-text-regular">Under Construction</p>
+    <a class="dm-serif-text-regular" href="https://www.google.com/search?q=Horizion+Landscaping+LLC">Google Profile</a>
 </main>
 
 <style>
@@ -15,9 +17,17 @@
         width: 100%;
         height: 100%;
         background-color: #dfdfdf;
-        display: grid;
+        /* display: grid;
         grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-        column-gap: 1px;
+        column-gap: 1px; */
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+    }
+
+    p {
+        font-size: 2rem;
     }
     
     section {
